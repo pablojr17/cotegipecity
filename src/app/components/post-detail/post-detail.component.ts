76fcf0d514cfb8f0aff8598dee56dc8a47aac182
pablojr17 <pablojr17@gmail.com>
2025-03-21 08:51:05 -0300

    update botão de voltar e analictts

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,21 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../../service/post.service';
-
+declare var gtag: Function;
 @Component({
   selector: 'app-post-detail',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './post-detail.component.html'
+  templateUrl: './post-detail.component.html',
+  styleUrl: './post-detail.component.scss'
 })
 export class PostDetailComponent implements OnInit {
   post: any;
 
-  constructor(private route: ActivatedRoute, private postService: PostService) {}
+  constructor(private route: ActivatedRoute, private postService: PostService, private location: Location) {}
 
   ngOnInit() {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.postService.getPostById(id).subscribe(data => this.post = data);
+  }
+
+  goBack() {
+    gtag('event', 'voltar_post', {
+      'event_category': 'Navegação',
+      'event_label': this.post?.title || 'Post Desconhecido',
+      'post_id': this.post?.id || 'N/A',
+      'post_titulo': this.post?.title || 'Sem título',
+      'post_url': window.location.href,
+      'post_tempo_visualizacao': Math.floor(performance.now() / 1000) + 's'
+    });
+
+    this.location.back();
   }
 }
