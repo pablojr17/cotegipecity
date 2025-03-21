6af3cc9f5285d339bc3019935aa51591c69116b4
pablojr17 <pablojr17@gmail.com>
2025-03-21 08:28:22 -0300

    update

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -4,8 +4,8 @@ import { Observable, of } from 'rxjs';
 @Injectable({ providedIn: 'root' })
 export class PostService {
   private posts = [
-    { id: 1, title: 'História de Cotegipe', content: 'Cotegipe tem uma rica história...', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG5Cs--HmVRCYOb0M-jvSi3HDXtjmPiUle2g&s' },
-    { id: 2, title: 'Senhor Tião - O faz-tudo', content: 'O Senhor Tião era conhecido por...', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgYhC3ypGiQtDIoZOzuLdzw_ceySBRrXnZ0fh2Sm0rLRUBcca3qq_pmGrBJKpY2c_gPCr317lk2gNl8E9TEeZiuI33kHh3AyQSivAojAJwqWDIUbMNv1f9dT9M9_AZe5V7ogFVFm-e1wTi9/s1600/TI%C3%83O+OLEIRO+98+ANOS_05.jpg' }
+    { id: 1, title: 'História de Cotegipe', content: 'História. A região era primitivamente habitada pelos índios acroás. O território integrava a sesmaria da Casa da Ponte. Seu povoamento iniciou-se na primeira metade do século XVIII, por aventureiros procedentes da Província de Pernambuco, que se estabeleceram à margem do Rio Grande, desenvolvendo a agropecuária.', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRG5Cs--HmVRCYOb0M-jvSi3HDXtjmPiUle2g&s' },
+    // { id: 2, title: 'Senhor Tião - O faz-tudo', content: 'O Senhor Tião era conhecido por...', image: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgYhC3ypGiQtDIoZOzuLdzw_ceySBRrXnZ0fh2Sm0rLRUBcca3qq_pmGrBJKpY2c_gPCr317lk2gNl8E9TEeZiuI33kHh3AyQSivAojAJwqWDIUbMNv1f9dT9M9_AZe5V7ogFVFm-e1wTi9/s1600/TI%C3%83O+OLEIRO+98+ANOS_05.jpg' }
   ];
 
   getPosts(): Observable<any[]> {
